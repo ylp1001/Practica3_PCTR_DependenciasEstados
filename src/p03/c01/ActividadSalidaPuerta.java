aae96dda99d655a5299dfd3adbb9130f7a86ea78
ylp1001 <yanestepar2000@gmail.com>
2022-03-26 17:12:43 +0100

    Salida parque

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -1,8 +1,28 @@
 package src.p03.c01;
 
-public class ActividadSalidaPuerta{
+public class ActividadSalidaPuerta implements Runnable{
 
-	//
-	// TODO
-	//
+	private static final int NUMSALIDAS = 20;
+	private String puerta;
+	private IParque parque;
+
+	public ActividadSalidaPuerta(String puerta, IParque parque) {
+		this.puerta = puerta;
+		this.parque = parque;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < NUMSALIDAS; i ++) {
+			try {
+				parque.SalirDelParque(puerta);
+				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5)*1000);
+			} catch (InterruptedException e) {
+				Logger.getGlobal().log(Level.INFO, "Entrada interrumpida");
+				Logger.getGlobal().log(Level.INFO, e.toString());
+				return;
+			}
+		}
+
+	}
 }
