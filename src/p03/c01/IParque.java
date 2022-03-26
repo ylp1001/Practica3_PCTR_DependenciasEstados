0d7c7710dcccba8c25bb22b05c75b309007e3222
ylp1001 <yanestepar2000@gmail.com>
2022-03-26 17:09:23 +0100

    Interface cambiada

--- src/p03/c01/IParque.java
+++ src/p03/c01/IParque.java
@@ -2,8 +2,8 @@ package src.p03.c01;
 
 public interface IParque {
 	
-	public abstract void entrarAlParque(String puerta);
+	public abstract void entrarAlParque(String puerta) throws InterruptedException;
+	public abstract void SalirDelParque(String puerta) throws InterruptedException;
 
-	// TODO
 
 }
