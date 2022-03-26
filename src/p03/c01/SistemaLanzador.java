c8e64e67c147f7b789d2768c0f5b3e4888014a01
erp1004 <elenarecioperez@gmail.com>
2022-03-26 17:38:33 +0100

    SistemaLanzador finalizado

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -3,7 +3,7 @@ package src.p03.c01;
 public class SistemaLanzador {
 	public static void main(String[] args) {
 		
-		IParque parque = new Parque(50); // TODO
+		IParque parque = new Parque(50); 
 		char letra_puerta = 'A';
 		
 		System.out.println("¡Parque abierto!");
